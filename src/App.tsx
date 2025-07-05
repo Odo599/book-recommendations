f3284ed8dc0aa664e54591f1790a09840655d6f8
odo599 <owen00064@gmail.com>
2025-07-05 16:50:16 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+    return (
+        <>
+            <h1>Book Recommendations</h1>
+            <div className="input_div"></div>
+            <button className="text-button">Get Recommendations</button>
+            <button className="text-button">
+                <b>+</b>
+            </button>
+        </>
+    );
+}
+
+export default App;
